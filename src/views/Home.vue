00fb286df35e9de73274d638c9444d0da58febb6
Raz <me@raz.works>
2021-09-03 22:50:40 +0200

    filled the home page with a brief explanation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,11 @@
 <template>
-  <div></div>
+  <h1 class="text-2xl font-bold">Nothing to see here 🤯</h1>
+  <p>
+    If you want to see an example on how you could retrieve some data from the
+    blockchain, go to the
+    <router-link class="text-green-500 font-bold" to="/account"
+      >Account Page</router-link
+    >. You need to be connected to Metamask to go there.
+  </p>
 </template>
 <script setup lang="ts"></script>
