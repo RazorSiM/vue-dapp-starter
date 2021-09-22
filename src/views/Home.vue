7884183fb09c71038754f77dd65b287bfc948bd8
Raz <me@raz.works>
2021-09-22 22:39:51 +0200

    update home description

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <h1 class="text-2xl font-bold">Nothing to see here 🤯</h1>
   <p>
-    If you want to see an example on how you could retrieve some data from the
-    blockchain, go to the
+    The Account page is route guarded. You need to be connected to metamask to
+    go there.
     <router-link class="text-green-500 font-bold" to="/account"
       >Account Page</router-link
-    >. You need to be connected to Metamask to go there.
+    >.
   </p>
-  <div>
+
+  <div class="mt-10">
     <p>
       This is an example on how you could use a composable to fetch some data
       using an external provider like Alchemy
