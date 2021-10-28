808c443c8d29ad454f85862249628c13b490ff5c
Raz <me@raz.works>
2021-10-29 00:31:54 +0200

    account change and network change event listeners

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,19 @@ const route = useRoute();
 let layout = $computed(() => {
   return route.meta.layout;
 });
+onMounted(() => {
+  //@ts-expect-error any
+  window.ethereum.on("accountsChanged", (accounts) => {
+    console.log("New account detected: ", accounts);
+    window.location.reload();
+  });
+  //@ts-expect-error any
+  window.ethereum.on("chainChanged", (chainId) => {
+    // Handle the new chain.
+    // Correctly handling chain changes can be complicated.
+    // We recommend reloading the page unless you have good reason not to.
+    console.log("New chain detected: ", chainId);
+    window.location.reload();
+  });
+});
 </script>
